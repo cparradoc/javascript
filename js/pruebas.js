2b481d4b0ce3caf127a31112e3ab44f7e3281588
cparradoc <cparradoc@minsait.com>
2022-02-15 10:16:54 +0100

    basic arquitecture

---
+++ js/pruebas.js
@@ -0,0 +1,20 @@
+var moderatII = new Array();
+        titanes[0] = "The Mark";
+        titanes[1] = "Bad Kingdom";
+        titanes[2] = "Versions";
+        titanes[3] = "Milk";
+        titanes[4] = "Gita";
+        titanes[5] = "Damage Done";
+        
+var moderat = new Array();
+    titanides[0] = "Rusty nails";
+    titanides[1] = "Seanmonkey";
+    titanides[2] = "Nasty silence";
+    titanides[3] = "Berlin";
+
+    moderatII.concat(moderat);
+
+    console.log(moderatII);
+    
+    console.log(moderat);
+
