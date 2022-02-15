7ea7ec90b73d1af1d2169569b314a7411f387ce0
cparradoc <cparradoc@minsait.com>
2022-02-15 17:06:25 +0100

    tests updated

--- js/pruebas.js
+++ js/pruebas.js
@@ -37,4 +37,72 @@ console.log(num1);
 console.log(numb);
 console.log(num2);
 
+var tengoDinero = true;
+var meDaMiedoVolar = true;
 
+// Puedo ir a Mexico si tengo dinero y NO me da miedo volar
+var puedoIrAMexico = tengoDinero && !meDaMiedoVolar;
+console.log(puedoIrAMexico);
+
+var meTomoUnTranquilizante = true;
+// Puedo ir a Mexico si tengo dinero y NO me da miedo volar o me tomo un tranquilizante
+var puedoIrAMexico = tengoDinero && (!meDaMiedoVolar || meTomoUnTranquilizante);
+console.log(puedoIrAMexico);
+
+
+var result1 = true == 1;
+var result2 = false == 0;
+var result3 = "1" == 1;
+var result4 = undefined == null;
+
+var result5 = true === 1;
+var result6 = false === 0;
+var result7 = "1" === 1;
+var result8 = undefined === null;
+
+var wtf = result6 === result7 || result8; 
+
+var score = 5;
+
+console.log(score >= 6 ? 'aprobado' : 'suspenso'); //operador ternario, no encadenarlos
+
+var myArray = ['Alberto', 'Mario', 'Jose', 'Juan'];
+
+var imprime = function(element) {
+  console.log(element);
+}
+
+myArray.forEach(imprime); //ejemplo de foreach pra no hacer un bucle for que recorra el array entero, pueden ser objetos no iterables
+
+var justiceLeague = ['Batman', 'Aquaman', 'Superman'];
+for (var justice of justiceLeague) { //tambien se pueden iterar con for of, aunque solo en ibjetos iterables
+  console.log(justice);
+}
+
+var dieHardArray = [1, 2,'Simon', 'John McClane', 'Zeus Carver'];
+
+var dieHardObj = {
+  name: 'John',
+  surname: 'McClane',
+  age: 37
+};
+ 
+// Iterar un Array
+for (value of dieHardArray) {
+  console.log(value);  
+}
+
+for (key in dieHardArray) {
+  console.log(key);  
+}
+
+ 
+// Iterar un Objeto
+
+for (key in dieHardObj) {
+  console.log(key);  
+}
+ 
+for (value of dieHardObj) {
+  console.log(value);  
+}
