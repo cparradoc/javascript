c9b0e165a39c331292f4da56cd15267cb52bccfc
cparradoc <cparradoc@minsait.com>
2022-02-16 16:49:59 +0100

    POO tests

--- js/pruebasPOO.js
+++ js/pruebasPOO.js
@@ -13,3 +13,35 @@ class Animal {
   var miGato = new Animal('Gato', 'Miau!');
   miPerro.emitirSonido(); // El Perro hace Guau!
   miGato.emitirSonido(); // El Gato hace Miau!
+
+  class Vehiculo {
+    constructor(tipo, nombre, ruedas) {
+      this.tipo = tipo;
+      this.nombre = nombre;
+      this.ruedas = ruedas;
+    }
+  
+    getRuedas() {
+      return this.ruedas;
+    }
+  
+    arrancar() {
+      console.log(`Arrancando el ${this.nombre}`);
+    }
+  
+    aparcar() {
+      console.log(`Aparcando el ${this.nombre}`);
+    }
+  }
+
+  class Coche extends Vehiculo { // extiende de Vehiculo
+    constructor(nombre, color) {
+      super('coche', nombre, 4); // ejecuta el constructor del padre
+      this.color = color;
+    }
+ }
+
+ var beetle = new Coche('Volkswagen Beetle', 'Rojo'); 
+var numRuedas = beetle.getRuedas() // 4
+var color = beetle.color;
+beetle.arrancar() // Arrancando el beetle
