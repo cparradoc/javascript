5b6fa07ec16d6a22ced50d9f58fd5006bdc401f1
cparradoc <cparradoc@minsait.com>
2022-02-16 17:46:49 +0100

    tests finished

--- js/pruebasPOO.js
+++ js/pruebasPOO.js
@@ -45,3 +45,68 @@ class Animal {
 var numRuedas = beetle.getRuedas() // 4
 var color = beetle.color;
 beetle.arrancar() // Arrancando el beetle
+
+class Moto extends Vehiculo { // extiende de Vehiculo
+  constructor(nombre, isSidecar = false) {
+    super('moto', nombre, 2); // ejecuta el constructor del padre
+    this.isSidecar = isSidecar;
+  }
+
+  tieneSidecar() {
+    return this.isSidecar;
+  }
+}
+
+var vespa = new Moto('Vespa');
+console.log(vespa.tieneSidecar());
+
+//metodos static: no necesitan una instancia creada para ser llamados
+
+class Utilidades {
+  constructor() { }
+
+  static calcularEdad(fechaNacimiento) {
+    const hoy = new Date();
+    let edad = hoy.getFullYear() - fechaNacimiento.getFullYear();
+
+    if (hoy.getMonth() < fechaNacimiento.getMonth()) {
+      edad--;
+    }
+
+    if ((fechaNacimiento.getMonth() === hoy.getMonth()) &&
+      (hoy.getDate() < fechaNacimiento.getDate())) {
+      edad--;
+    }
+
+    return edad;
+  }
+
+}
+let fechaNacimiento = new Date(1991, 5, 8);
+let edad = Utilidades.calcularEdad(fechaNacimiento);
+console.log(edad); // 28
+
+//Getters y setters
+
+class Coche {
+  constructor(marca, potencia) {
+    this._marca = marca;
+    this._potencia = potencia;
+  }
+
+  get marca() {
+    return `marca: ${this._marca}`;
+  }
+
+  set potencia(value) {
+    if (typeof value === 'number') {
+      this._potencia = value;
+    } else {
+      console.error('la potencia debe ser numérica');
+    }
+  }
+}
+
+var ibiza = new Coche('seat', 105);
+ibiza.potencia = 'Mucha'; // ERROR: la potencia debe ser numérica
+
