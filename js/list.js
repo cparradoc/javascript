44cd8977570b2783109b919182552b52cd4c21c8
cparradoc <cparradoc@minsait.com>
2022-02-18 12:28:47 +0100

    list pokemon with import

--- js/list.js
+++ js/list.js
@@ -1,4 +1,5 @@
-class Pokemon{
+
+export class Pokemon{
     constructor(id, name) {
         this.id = id;
         this.name = name;
@@ -6,22 +7,10 @@ class Pokemon{
         const image = document.createElement("img");
         image.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + id + ".png";
         this.image = image;
-
-
     }
 }
 
-
-fetch('https://pokeapi.co/api/v2/pokemon/')
-    .then((response) => {
-        return response.json();
-    })
-    .catch(error => console.error('Error:', error))
-    .then(data => {
-        console.log(data);
-    });
-
-function list() {
+export function list() {
     fetch('https://pokeapi.co/api/v2/pokemon/')
     .then((response) => {
         return response.json();
@@ -31,28 +20,28 @@ function list() {
         console.log(data);
         let pokedex = [];
 
-        for(pokemon in data.results) {
-            idPokemon = data.results[pokemon].url.split('/');
+        for(var pokemon in data.results) {
+            var idPokemon = data.results[pokemon].url.split('/');
             pokemon = new Pokemon(idPokemon[idPokemon.length - 2], data.results[pokemon].name);
             pokedex.push(pokemon);
         }
-        console.log(pokedex);
 
-        let divList = document.getElementsByClassName("container")[1];
+        let divList = document.getElementsByClassName("container__list")[0];
 
         for(pokemon in pokedex) {
-            divPokemonListed = document.createElement("div");
+            let divPokemonListed = document.createElement("div");
             divPokemonListed.clasName = "container__pokemon";
             divPokemonListed.id = pokedex[pokemon].id;
 
-            textNodeNamePokemon = document.createTextNode(pokedex[pokemon].name);
-            namePokemonListed = document.createElement("h2");
+            let textNodeNamePokemon = document.createTextNode(pokedex[pokemon].name);
+            let namePokemonListed = document.createElement("h2");
             namePokemonListed.appendChild(textNodeNamePokemon);
 
             divPokemonListed.appendChild(namePokemonListed);
 
             divList.appendChild(divPokemonListed);
             divList.appendChild(pokedex[pokemon].image);
+
         }
 
 
