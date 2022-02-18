465f51d329d5b7864e32cb2b3e1b97ef96a1c1c6
cparradoc <cparradoc@minsait.com>
2022-02-18 15:03:29 +0100

    detail (not working)

--- js/detail.js
+++ js/detail.js
@@ -1,3 +1,12 @@
-function onListClicked() {
-    document.getElementsByClassName("header__list")[0];
- }
+
+function detail(idPokemon) {
+    fetch('https://pokeapi.co/api/v2/pokemon/' + idPokemon)
+    .then((response) => {
+        return response.json();
+    })
+    .catch(error => console.error('Error:', error))
+    .then(data => {
+        let divList = document.getElementsByClassName("main-content__list")[0];
+        mainContent.innerHTML = '';
+    })
+}
