f3686d3367b35a419f6de2d910376655c2fb9faa
cparradoc <cparradoc@minsait.com>
2022-02-21 09:52:01 +0100

    basic div pokemon detail

--- js/detail.js
+++ js/detail.js
@@ -6,7 +6,22 @@ function detail(idPokemon) {
     })
     .catch(error => console.error('Error:', error))
     .then(data => {
-        let divList = document.getElementsByClassName("main-content__list")[0];
-        mainContent.innerHTML = '';
+        let divList = document.getElementsByClassName("container-pokemon")[0];
+        divList.innerHTML = '';
+
+        let divPokemonConteinerDetail = document.createElement("div");
+        divPokemonConteinerDetail.className = "container-pokemon__list-display";
+
+        let divPokemonDetailed = document.createElement("div");
+        divPokemonDetailed.className = "container-pokemon__pokemon";
+        let pokemonName = document.createElement("h2");
+        let textNodeNamePokemon = document.createTextNode(pokedex[pokemon].name);
+        pokemonName.appendChild(textNodeNamePokemon);
+        divPokemonConteinerDetail.appendChild(pokemonName);
+
+        
+        
+
+
     })
 }
